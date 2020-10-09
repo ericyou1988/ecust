a363767385c5b41c9165e71b4b07ab87094d5897
ericyou1988 <eric_you1988@outlook.com>
2020-10-09 11:02:49 +0800

    update 20201009

--- test.py
+++ test.py
@@ -1,111 +1,12 @@
-#!/usr/bin/python
-# coding:utf-8
+from selenium import webdriver
+import time
 
-import tensorflow as tf
-import input_data
-# 加载数据
-mnist = input_data.read_data_sets('Mnist_data', one_hot=True)
+def main():
+    chrome_driver = 'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe'  #chromedriver的文件位置
+    b = webdriver.Chrome(executable_path = chrome_driver)
+    b.get('https://www.google.com')
+    time.sleep(5)
+    b.quit()
 
-# x不是一个特定的值，而是一个占位符
-# 能够输入任意数量的MNIST图像，每一张图展平成784维的向量
-x = tf.placeholder("float", [None, 784])
-#  一个Variable代表一个可修改的张量
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
-# y=softmax(Wx+b)
-y = tf.nn.softmax(tf.matmul(x, W)+b)
-# 添加一个新的占位符用于输入正确值
-y_ = tf.placeholder("float", [None, 10])
-# 计算交叉熵
-# 用tf.reduce_sum 计算张量的所有元素的总和
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-# 以0.01的学习速率最小化交叉熵
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-
-# 在Session里面启动模型
-sess = tf.Session()
-# 初始化变量
-init = tf.initialize_all_variables()
-sess.run(init)
-
-# 让模型循环训练1000次
-for i in range(1000):
-    # 随机抓取训练数据中的100个批处理数据点
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    # 用这些数据点作为参数替换之前的占位符来运行train_step
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-
-# 检测预测是否与实际标签匹配,返回一组布尔值
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-# 把布尔值转换成浮点数，然后取平均值
-# [True, False, True, True]变成[1,0,1,1],平均后得0.75
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-# 计算所学习到的模型在测试数据集上面的正确率
-print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
-
-
-####先期函数定义
-# 权重初始化
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-
-
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-
-# 卷积和池化
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-
-####模型构建
-# c1
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-x_image = tf.reshape(x, [-1, 28, 28, 1])
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
-# c2
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-# f1
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
-
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-# Dropout
-keep_prob = tf.placeholder("float")
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-# out
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
-
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
-
-####训练和评估模型
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-sess.run(tf.initialize_all_variables())
-for i in range(20000):
-    batch = mnist.train.next_batch(50)
-    if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
-            x: batch[0], y_: batch[1], keep_prob: 1.0})
-        print("step %d, training accuracy %g" % (i, train_accuracy))
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-print("test accuracy %g" % accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+if __name__ == '__main__':
+    main()
